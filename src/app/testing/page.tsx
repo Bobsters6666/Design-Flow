3c6afc9a89d5b937194d49819e206ab20d51cc42
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-11-20 19:35:20 +1300

    commented out testing block

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,46 +1,46 @@
-"use client";
+// "use client";
 
-import React, { useState } from "react";
-import "./testing.css";
-import { AnimatePresence, motion } from "framer-motion";
-const page = () => {
-  const [showH2, setShowH2] = useState(true);
+// import React, { useState } from "react";
+// import "./testing.css";
+// import { AnimatePresence, motion } from "framer-motion";
+// const page = () => {
+//   const [showH2, setShowH2] = useState(true);
 
-  return (
-    <div>
-      <div className="py-96">
-        <AnimatePresence>
-          {showH2 && (
-            <motion.h2
-              key={1}
-              className="text-center text-3xl"
-              initial={{ x: "-100vw", opacity: 0.3 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1.5, type: "spring", stiffness: 80 }}
-              exit={{ x: "100vw", opacity: 0.3 }}
-            >
-              Hello guys
-            </motion.h2>
-          )}
+//   return (
+//     <div>
+//       <div className="py-96">
+//         <AnimatePresence>
+//           {showH2 && (
+//             <motion.h2
+//               key={1}
+//               className="text-center text-3xl"
+//               initial={{ x: "-100vw", opacity: 0.3 }}
+//               animate={{ x: 0, opacity: 1 }}
+//               transition={{ duration: 1.5, type: "spring", stiffness: 80 }}
+//               exit={{ x: "100vw", opacity: 0.3 }}
+//             >
+//               Hello guys
+//             </motion.h2>
+//           )}
 
-          {!showH2 && (
-            <motion.h2
-              key={2}
-              className="text-center text-3xl"
-              initial={{ x: "-100vw", opacity: 0.3 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1.5, type: "spring", stiffness: 80 }}
-              exit={{ x: "100vw", opacity: 0.3 }}
-            >
-              Exit leave haha
-            </motion.h2>
-          )}
-        </AnimatePresence>
+//           {!showH2 && (
+//             <motion.h2
+//               key={2}
+//               className="text-center text-3xl"
+//               initial={{ x: "-100vw", opacity: 0.3 }}
+//               animate={{ x: 0, opacity: 1 }}
+//               transition={{ duration: 1.5, type: "spring", stiffness: 80 }}
+//               exit={{ x: "100vw", opacity: 0.3 }}
+//             >
+//               Exit leave haha
+//             </motion.h2>
+//           )}
+//         </AnimatePresence>
 
-        <button onClick={() => setShowH2(!showH2)}>Toggle</button>
-      </div>
-    </div>
-  );
-};
+//         <button onClick={() => setShowH2(!showH2)}>Toggle</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
